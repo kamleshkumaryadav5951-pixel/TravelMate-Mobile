6d274e2cc2a03f43ef67a174b77b5827007646e0
kamlesh kumar yadav <kamleshkumaryadav5951@gmail.com>
2026-09-03 22:18:35 +0530

    Remove temporary scripts and backup files from repository

--- fix_toggle.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/screens/Itinerary.tsx', 'utf-8');
-
-content = content.replace(
-  "      newDays[activeDay].activities[activityIndex].done = !newDays[activeDay].activities[activityIndex].done;",
-  "      newDays[activeDay].activities[activityIndex] = { ...newDays[activeDay].activities[activityIndex] };\n      newDays[activeDay].activities[activityIndex].done = !newDays[activeDay].activities[activityIndex].done;\n      console.log('Toggled activity', activityIndex, newDays[activeDay].activities[activityIndex].done);"
-);
-
-fs.writeFileSync('src/screens/Itinerary.tsx', content);
